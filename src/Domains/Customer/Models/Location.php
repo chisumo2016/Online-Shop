e263355a47cd360e5bfd3bfbaad6051c19536855
chisumo <bchisumo74@yahoo.com>
2021-09-22 15:56:22 +0100

    DDM in location to add some logic

---
+++ src/Domains/Customer/Models/Location.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Domains\Customer\Models;
+
+use Database\Factories\LocationFactory;
+use Domains\Customer\Models\Concerns\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasUuid;
+    use HasFactory;
+
+    protected $fillable =[
+        'uuid',
+        'house',
+        'street',
+        'parish',
+        'ward',
+        'district',
+        'county',
+        'country',
+    ];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return  new LocationFactory();
+    }
+}
