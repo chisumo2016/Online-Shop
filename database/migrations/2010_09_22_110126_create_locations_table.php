cb3168db10fa97e60079563384d576e0a4e760d9
chisumo <bchisumo74@yahoo.com>
2021-09-22 15:49:22 +0100

    Alter the field year on locations migrations

---
+++ database/migrations/2010_09_22_110126_create_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up() : void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->string('house');
+            $table->string('street');
+            $table->string('parish')->nullable();
+            $table->string('ward')->nullable();
+            $table->string('district')->nullable();
+            $table->string('county')->nullable();
+            $table->string('postcode');
+            $table->string('country');
+
+            $table->timestamps();
+        });
+    }
+
+
+    public function down():void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
