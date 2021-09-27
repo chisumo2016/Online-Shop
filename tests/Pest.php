611e57338c658d0d2881abeb0841795465bfd361
chisumo <bchisumo74@yahoo.com>
2021-09-27 13:53:26 +0100

    Installed Pest

---
+++ tests/Pest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Tests\TestCase;
+
+uses(TestCase::class)->in('Feature');
+
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
+
+
+function something()
+{
+    // ..
+}
