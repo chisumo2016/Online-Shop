e798e996642a5e8e562bdab719cca207b284b542
chisumo <bchisumo74@yahoo.com>
2021-11-16 15:08:12 +0000

    summary Notes

---
+++ larav.php
@@ -0,0 +1,22 @@
+New Laravel
+Read Larvel Blog
+
+Enum
+ScopeBinding()
+Can('', '')
+withoutMiddleware('')
+collect()
+
+RELATIONSHIP
+Post::where('category_id', $category->id)->get();
+Post::whereBelongsTo($category)
+      ->whereBelongsTo($author)
+      ->get();
+
+TESTING
+-LazilyRefreshDatabase
+
+REDUCE COLLECTION METHOD
+ -Check documentation
+
+composer octane
